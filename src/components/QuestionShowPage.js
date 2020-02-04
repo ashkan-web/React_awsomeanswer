62169b4719113695d692ce8686756d5741e6936a
ashkan <ashkanh.info@gmail.com>
2020-02-04 11:11:02 -0800

    add one file per components

---
+++ src/components/QuestionShowPage.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+import { QuestionDetails } from "./QuestionDetails";
+import { AnswerDetails } from "./AnswerDetails";
+
+const QuestionShowPage = () => {
+  return (
+    <div>
+      <QuestionDetails
+        title="What is your favourite color?"
+        body="Red, Green, Magenta, etc."
+        author={{ full_name: "Michael Owen" }}
+        view_count={100}
+        created_at={new Date()}
+      />
+      <AnswerDetails
+        body="Green"
+        author={{ full_name: "Dylan O'Brien " }}
+        created_at={new Date()}
+      />
+    </div>
+  );
+};
+
+export default QuestionShowPage;
