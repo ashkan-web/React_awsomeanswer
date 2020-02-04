c82cde285f30834896c17870e44399b801fc2f45
ashkan <ashkanh.info@gmail.com>
2020-02-04 10:22:48 -0800

    first React index page

--- src/index.js
+++ src/index.js
@@ -1,10 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// A react component is a function that returns a react element.
+// PascalCase is the naming convention for react components.
+// Components whose names does not begin with a capital letter,
+// will be interpretted as a plain HTML tag.
+
+const QuestionDetails = () => {
+  return (
+    <div>
+      <h2>What is your favourite color?</h2>
+      <p>
+        Red, green, magenta, etc. <br />
+        By Michael Owen
+      </p>
+      <small>Seen 10 times - Created 10 days ago</small>
+    </div>
+  );
+};
+
+// In JSX, self-closing tags must be closed. <img> doesn't work,
+// you must write <img />
+
+ReactDOM.render(<QuestionDetails />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
