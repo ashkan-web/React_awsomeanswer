04f66778fe059b91fb18d55b3f370350b9885109
ashkan <ashkanh.info@gmail.com>
2020-02-06 10:53:31 -0800

    install route-react and route the welcome page

---
+++ src/components/WelcomePage.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+export const WelcomePage = props => (
+  <div className="Page">
+    <div className="ui header">Awesome Answers</div>
+    <div className="ui teal segment">WElcome to Awesome Answers React</div>
+  </div>
+);
